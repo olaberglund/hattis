4b8a6b549c23afc5668a9e2e5813800d7f5616de
Ola Berglund <olakjberglund@gmail.com>
2023-11-11 23:50:06 +0100

    use Vector.length, cus' O(1)

--- app/Reseto.hs
+++ app/Reseto.hs
@@ -14,7 +14,7 @@ solve [n, k] = cross (V.enumFromTo @Int 2 n) (k - 1)
 cross :: Vector Int -> Int -> Int
 cross xs k =
   let (yes, no) = V.partition ((== 0) . flip mod (V.head xs)) xs
-      len = length yes
+      len = V.length yes
    in if k > len - 1
         then cross no (k - len)
         else yes ! k
