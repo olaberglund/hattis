626dcda4921c514fb08151d8bfe11483c44ca7d1
Ola Berglund <olakjberglund@gmail.com>
2023-11-19 15:41:50 +0100

    tildes: union-find in haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,68 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Data.Bits
-import Data.IntMap (IntMap, assocs, fromList, insert, (!))
-import Data.List (foldl')
+import Data.Array.IO (IOUArray)
+import Data.Array.MArray
+import Data.Foldable (traverse_)
+import Data.Text hiding (head, map, tail)
+import Data.Text.IO
+import Data.Text.Read
+import Prelude hiding (getContents, lines, read, words)
 
 main :: IO ()
-main = interact $ show . solve . map (map read . words) . lines
+main = do
+  wordRows <- map words . lines <$> getContents
+  uf <- newUnionFind (decimalU (head (head wordRows)) + 1)
+  traverse_ (act uf . mkQuery) (tail wordRows)
 
-solve :: [[Int]] -> Int
-solve ([n, _] : rest) = snd $ until ((== shiftL 1 n) . fst) (step constraints) (0, 0)
-  where
-    constraints = foldl' mkConstraint (empty n) rest
+data Query = Merge !Int !Int | Size !Int deriving (Show)
 
-step :: IntMap Int -> (Int, Int) -> (Int, Int)
-step m (pat, cnt) =
-  if any (invalid pat) (assocs m)
-    then (pat + 1, cnt)
-    else (pat + 1, cnt + 1)
-  where
-    invalid pat (k, v)
-      | pat `testBit` k = pat .&. v /= 0
-      | otherwise = False
+mkQuery :: [Text] -> Query
+mkQuery ["t", a, b] = Merge (decimalU a) (decimalU b)
+mkQuery ["s", a] = Size (decimalU a)
 
-mkConstraint :: IntMap Int -> [Int] -> IntMap Int
-mkConstraint m [a, b] =
-  let a' = a - 1
-      b' = b - 1
-   in insert b' (m ! b' `setBit` a') (insert a' (m ! a' `setBit` b') m)
+decimalU :: Text -> Int
+decimalU = fst . either error id . decimal
 
-empty :: Int -> IntMap Int
-empty max = fromList $ map (,0) [0 .. max - 1]
+act :: UnionFind -> Query -> IO ()
+act uf (Merge a b) = unite uf a b
+act uf (Size a) = do
+  i <- root uf a
+  sz <- readArray (szs uf) i
+  print sz
+
+data UnionFind = UnionFind
+  { ids :: IOUArray Int Int,
+    szs :: IOUArray Int Int
+  }
+
+newUnionFind :: Int -> IO UnionFind
+newUnionFind n = UnionFind <$> newListArray (0, n - 1) [0 .. n - 1] <*> newArray (0, n - 1) 1
+
+root :: UnionFind -> Int -> IO Int
+root uf i = do
+  id <- readArray (ids uf) i
+  if id /= i
+    then do
+      gpid <- readArray (ids uf) id
+      writeArray (ids uf) i gpid
+      root uf id
+    else return i
+
+unite :: UnionFind -> Int -> Int -> IO ()
+unite uf p q = do
+  i <- root uf p
+  j <- root uf q
+  szi <- readArray (szs uf) i
+  szj <- readArray (szs uf) j
+  if i == j
+    then return ()
+    else
+      if szi < szj
+        then do
+          writeArray (ids uf) i j
+          writeArray (szs uf) j (szi + szj)
+        else do
+          writeArray (ids uf) j i
+          writeArray (szs uf) i (szj + szi)
