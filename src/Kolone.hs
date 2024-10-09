001cbc50cbfe51451d002e74932d65b72efebcb7
Ola Berglund <olakjberglund@gmail.com>
2024-10-09 21:27:55 +0200

    kolone'

--- src/Kolone.hs
+++ src/Kolone.hs
@@ -1,7 +1,9 @@
 module Kolone where
 
 import           Control.Arrow ((>>>))
+import           Data.Foldable (toList)
 import           Data.List     (foldl')
+import           Data.Sequence (Seq, empty, (|>))
 
 type Ant = Char
 
@@ -18,18 +20,18 @@ main =
                 )
 
 solve :: [Ant] -> [Ant] -> Int -> [Ant]
-solve as1 as2 t = merge $ zip as1' as2'
+solve as1 as2 t = toList $ merge $ zip as1' as2'
   where
-    merge :: [(Maybe Ant, Maybe Ant)] -> [Ant]
-    merge = foldl' merge' []
+    merge :: [(Maybe Ant, Maybe Ant)] -> Seq Ant
+    merge = foldl' merge' empty
 
-    merge' :: [Ant] -> (Maybe Ant, Maybe Ant) -> [Ant]
+    merge' :: Seq Ant -> (Maybe Ant, Maybe Ant) -> Seq Ant
     merge' ants pair =
-        ants <> case pair of
-            (Just a1, Just a2) -> [a2, a1]
-            (Nothing, Just a)  -> [a]
-            (Just a, Nothing)  -> [a]
-            _                  -> []
+        case pair of
+            (Just a1, Just a2) -> ants |> a2 |> a1
+            (Nothing, Just a)  -> ants |> a
+            (Just a, Nothing)  -> ants |> a
+            _                  -> ants
 
     nothings = replicate (length as2) Nothing
     as1' = nothings <> map pure (reverse as1) <> nothings
