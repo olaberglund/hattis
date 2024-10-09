e61d2c03f9714d52b815a6c70a351bc00e2c4ae4
Ola Berglund <olakjberglund@gmail.com>
2024-10-09 21:38:20 +0200

    kolone''

--- src/Kolone.hs
+++ src/Kolone.hs
@@ -1,9 +1,7 @@
 module Kolone where
 
 import           Control.Arrow ((>>>))
-import           Data.Foldable (toList)
 import           Data.List     (foldl')
-import           Data.Sequence (Seq, empty, (|>))
 
 type Ant = Char
 
@@ -13,26 +11,35 @@ main =
         lines
             >>> tail
             >>> ( \[ants1, ants2, t] ->
-                    solve
+                    simulate
                         ants1
                         ants2
                         (read t)
                 )
 
-solve :: [Ant] -> [Ant] -> Int -> [Ant]
-solve as1 as2 t = toList $ merge $ zip as1' as2'
+simulate :: [Ant] -> [Ant] -> Int -> [Ant]
+simulate left right t = foldl' (\ants _ -> go ants) (reverse left ++ right) [1 .. t]
   where
-    merge :: [(Maybe Ant, Maybe Ant)] -> Seq Ant
-    merge = foldl' merge' empty
+    go [] = []
+    go [x] = [x]
+    go (x : y : xs)
+        | x `elem` left && y `elem` right = y : x : go xs
+        | otherwise = x : go (y : xs)
 
-    merge' :: Seq Ant -> (Maybe Ant, Maybe Ant) -> Seq Ant
-    merge' ants pair =
-        case pair of
-            (Just a1, Just a2) -> ants |> a2 |> a1
-            (Nothing, Just a)  -> ants |> a
-            (Just a, Nothing)  -> ants |> a
-            _                  -> ants
-
-    nothings = replicate (length as2) Nothing
-    as1' = nothings <> map pure (reverse as1) <> nothings
-    as2' = replicate (length as1 + length as2 - t) Nothing <> map pure as2 <> replicate t Nothing
+-- solve :: [Ant] -> [Ant] -> Int -> [Ant]
+-- solve as1 as2 t = toList $ merge $ zip as1' as2'
+--   where
+--     merge :: [(Maybe Ant, Maybe Ant)] -> Seq Ant
+--     merge = foldl' merge' empty
+--
+--     merge' :: Seq Ant -> (Maybe Ant, Maybe Ant) -> Seq Ant
+--     merge' ants pair =
+--         case pair of
+--             (Just a1, Just a2) -> ants |> a2 |> a1
+--             (Nothing, Just a)  -> ants |> a
+--             (Just a, Nothing)  -> ants |> a
+--             _                  -> ants
+--
+--     nothings = replicate (length as2) Nothing
+--     as1' = nothings <> map pure (reverse as1) <> nothings
+--     as2' = replicate (length as1 + length as2 - t) Nothing <> map pure as2 <> replicate t Nothing
