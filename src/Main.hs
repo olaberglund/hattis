28562909284517f018b91f75d889284127628da6
olaberglund <ola@kronor.io>
2024-10-18 20:43:23 +0200

    restore  main

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,8 @@
-{-# LANGUAGE TupleSections #-}
-
 module Main where
 
-import           Control.Arrow ((>>>))
+import Control.Arrow ((>>>))
 
 main :: IO ()
 main =
-    interact $
-        lines
-            >>> head
-            >>> words
-            >>> map read
-            >>> ([],)
-            >>> iterate bubbleStep
-            >>> take 6
-            >>> map (uncurry (<>))
-            >>> map (unwords . map show)
-            >>> unlines
-
--- bubbleStep :: ([Int], [Int]) -> ([Int], [Int])
-bubbleStep :: ([Int], [Int]) -> ([Int], [Int])
-bubbleStep (prev, x : y : rest)
-    | x > y = (prev <> [y], x : rest)
-    | otherwise = bubbleStep (prev <> [x], y : rest)
-bubbleStep (prev, xs) = ([], prev <> xs)
+  interact $
+    lines >>> undefined
