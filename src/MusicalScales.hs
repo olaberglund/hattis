3ccbb12f2d8f5ba2392f477c47d0a067a5245121
Ola Berglund <olakjberglund@gmail.com>
2024-10-20 14:01:23 +0200

    musical scales'

--- src/MusicalScales.hs
+++ src/MusicalScales.hs
@@ -2,63 +2,44 @@
 
 module MusicalScales where
 
-import Control.Arrow ((&&&), (>>>))
-import Data.List (mapAccumL)
-import Data.Set (Set)
-import qualified Data.Set as Set
+import           Control.Arrow ((>>>))
+import           Data.Foldable (toList)
+import           Data.List     (scanl')
+import           Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
+import           Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import           Data.Set      (Set)
+import qualified Data.Set      as Set
 
-data Note = A | ASharp | B | C | CSharp | D | DSharp | E | F | FSharp | G | GSharp
-  deriving (Show, Eq, Ord, Enum, Read)
+notes :: Seq String
+notes = Seq.fromList ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
-nextNote :: Note -> Note
-nextNote GSharp = A
-nextNote n = succ n
-
-showNote :: Note -> String
-showNote n = case n of
-  ASharp -> "A#"
-  CSharp -> "C#"
-  DSharp -> "D#"
-  FSharp -> "F#"
-  GSharp -> "G#"
-  _ -> show n
-
-parseNote :: String -> Note
-parseNote n = case n of
-  "A#" -> ASharp
-  "C#" -> CSharp
-  "D#" -> DSharp
-  "F#" -> FSharp
-  "G#" -> GSharp
-  _ -> read n
-
-mkScale :: Note -> [Note]
-mkScale n = snd $ mapAccumL (\cn s -> (s cn, s cn)) n scale
- where
-  scale = [id, tone, tone, semi, tone, tone, tone, semi]
-  tone = nextNote . nextNote
-  semi = nextNote
-
-notes :: [String] -> Set Note
-notes = Set.fromList . map parseNote
-
-allScales :: [(Note, Set Note)]
-allScales = map (id &&& Set.fromList . mkScale) [A ..]
-
-scales :: Set Note -> [Note]
-scales ns = map fst $ filter (Set.isSubsetOf ns . snd) allScales
+scales :: Map String (Set String)
+scales =
+    Map.fromList
+        . toList
+        . Seq.mapWithIndex
+            ( \i n ->
+                (n, Set.fromList $ Seq.index notes <$> indices i)
+            )
+        $ notes
+  where
+    indices i = map (`mod` len) (scanl' (flip ($)) i [id, tone, tone, semi, tone, tone, tone, semi])
+    len = Seq.length notes
+    semi = (+ 1)
+    tone = semi . semi
 
 main :: IO ()
 main =
-  interact $
-    lines
-      >>> drop 1
-      >>> head
-      >>> words
-      >>> notes
-      >>> scales
-      >>> ( \case
-              [] -> ["none"]
-              ns -> map showNote ns
-          )
-      >>> unwords
+    interact $
+        lines
+            >>> drop 1
+            >>> head
+            >>> words
+            >>> Set.fromList
+            >>> (\s -> Map.filter (s `Set.isSubsetOf`) scales)
+            >>> Map.keys
+            >>> \case
+                [] -> "none"
+                ss -> unwords ss
