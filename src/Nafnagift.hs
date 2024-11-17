3bf68f9e442f3dbd094b4547e4a702cf22cc7a2b
olaberglund <ola@kronor.io>
2024-11-17 14:38:55 +0100

    nafnagift'

--- src/Nafnagift.hs
+++ src/Nafnagift.hs
@@ -1,24 +1,36 @@
 module Nafnagift where
 
 import Control.Arrow ((>>>))
-import Data.Either (isLeft)
-import Data.Foldable (maximumBy)
+import Data.Array (Array, Ix (..), listArray, (!))
+import Data.Foldable (Foldable (toList), minimumBy)
 import Data.Ord (comparing)
+import Data.Sequence ((<|))
+import qualified Data.Sequence as Seq
 
 main :: IO ()
-main =
-  interact $
-    lines >>> (\[m, n] -> scs m n) >>> combine
+main = interact $ lines >>> (\[m, n] -> solve m n)
 
-scs :: (Eq a) => [a] -> [a] -> [Either a a]
-scs [] ys = Right <$> ys
-scs xs [] = Right <$> xs
-scs (x : xs) (y : ys)
-  | x == y = Left x : scs xs ys
-  | otherwise =
-      maximumBy
-        (comparing (length . filter isLeft))
-        [Right y : scs (x : xs) ys, Right x : scs xs (y : ys)]
+solve :: (Eq a) => [a] -> [a] -> [a]
+solve m n = toList $ lcs (0, 0)
+ where
+  rng = ((0, 0), (length m, length n))
+  m' = listArray (0, length m) m
+  n' = listArray (0, length n) n
 
-combine :: [Either a a] -> [a]
-combine = map (either id id)
+  lcs = memo rng $ \(i, j) ->
+    let x = m' ! i
+        y = n' ! j
+        i' = i + 1
+        j' = j + 1
+     in if inRange rng (i', j')
+          then
+            if x == y
+              then x <| lcs (i', j')
+              else minimumBy (comparing length) [y <| lcs (i, j'), x <| lcs (i', j)]
+          else Seq.fromList $ drop i m <> drop j n
+
+tabulate :: (Ix i) => (i, i) -> (i -> e) -> Array i e
+tabulate rng f = listArray rng (map f $ range rng)
+
+memo :: (Ix i) => (i, i) -> (i -> e) -> i -> e
+memo rng = (!) . tabulate rng
